0771638787097540c9bb16b6285e859743ca87b7
nutmommam9 <nutmommam_9@hotmail.com>
2023-04-25 18:06:29 +0700

    Challenge 10

--- 10 - Kanban Board/start.js
+++ 10 - Kanban Board/start.js
@@ -1,4 +1,38 @@
 (() => {
   // เริ่มเขียนโค้ด
+  let draggingElem;
 
+  function onDrageStart() {
+    draggingElem = this; // this ดึงมาจาก taskElem
+  }
+
+  function onDrop() {
+    this.append(draggingElem);
+    draggingElem = null;
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+  }
+
+  function onDragEnter(event) {
+    event.preventDefault();
+  }
+
+  function run() {
+    const taskElem = Array.from(document.querySelectorAll('.task'));
+    const dropZoneElem = Array.from(document.querySelectorAll('.drop-zone'));
+
+    taskElem.forEach((taskElem) => {
+      taskElem.addEventListener('dragstart', onDrageStart);
+    });
+
+    dropZoneElem.forEach((dropZoneElem) => {
+      dropZoneElem.addEventListener('drop', onDrop);
+      dropZoneElem.addEventListener('dragover', onDragOver);
+      dropZoneElem.addEventListener('dragenter', onDragEnter);
+    });
+  }
+
+  run();
 })();
