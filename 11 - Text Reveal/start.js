1909b1e3678fc1e08a2b0dd12441c063402585a2
nutmommam9 <nutmommam_9@hotmail.com>
2023-04-25 21:40:14 +0700

    Challenge 11

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,4 +1,24 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function onScroll(){
+    const sectionElem = Array.from(document.querySelectorAll('section'));
 
+    sectionElem.forEach((sectionElem) => {
+      const imgElem = sectionElem.querySelector('img');
+      const textElem = sectionElem.querySelector('.text');
+
+      const scrollPosition = window.pageYOffset;
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+
+      if (scrollPosition >= revealPosition) {
+        textElem.classList.add('reveal');
+      }
+    })
+  };
+
+  function run(){
+    document.addEventListener('scroll', onScroll);
+  }
+
+  run();
 })();
