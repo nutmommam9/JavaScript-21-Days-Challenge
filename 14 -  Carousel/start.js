01647565ac2bf8ccea52d58b1896961d399ceb19
nutmommam9 <nutmommam_9@hotmail.com>
2023-04-26 15:36:28 +0700

    Challenge 14

--- 14 -  Carousel/start.js
+++ 14 -  Carousel/start.js
@@ -1,4 +1,30 @@
 (() => {
   // เริ่มเขียนโค้ด
+let currentIndex = 0;
 
+  function dispalyImage(imageElems, newIndex){
+    const lastIndex = imageElems.length - 1
+
+    if(newIndex < 0) {
+      newIndex = lastIndex;
+    } else if(newIndex > lastIndex) {
+      newIndex = 0;
+    }
+
+    const newImage = imageElems[newIndex];
+    newImage.scrollIntoView({ behavior: 'smooth' });
+
+    currentIndex = newIndex;
+  }
+
+  function run(){
+    const imageElems = document.querySelectorAll('img');
+    const previousBtnElem = document.querySelector('.previous');
+    const nextBtnElem = document.querySelector('.next');
+
+    previousBtnElem.addEventListener('click', () => dispalyImage(imageElems, currentIndex - 1))
+    nextBtnElem.addEventListener('click', () => dispalyImage(imageElems, currentIndex + 1))
+  }
+
+  run();
 })();
