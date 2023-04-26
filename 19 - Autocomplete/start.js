068030633bb7fc00fdbe1c8f79e9b60648774562
nutmommam9 <nutmommam_9@hotmail.com>
2023-04-26 18:36:52 +0700

    Challenge 19

--- 19 - Autocomplete/start.js
+++ 19 - Autocomplete/start.js
@@ -1,3 +1,52 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const carBrands = [
+    "BMW",
+    "Maserati",
+    "Mercedes Benz",
+    "Ferrari",
+    "Toyota",
+    "Honda",
+    "Hyundai",
+  ];
+
+  const searchElem = document.querySelector(".search");
+
+  function clearResults(){
+    const ulElem = document.querySelector('.results');
+    if(ulElem) {
+      document.body.removeChild(ulElem);
+    }
+  }
+
+  function selectCarBrand(event){
+    searchElem.value = event.target.innerText;
+    clearResults();
+  }
+
+  function onInput(event) {
+    const inputText = event.target.value.toLowerCase();
+
+    const matchCarBrands = carBrands.filter((carBrand) =>
+      carBrand.toLowerCase().startsWith(inputText)
+    );
+
+    const ulElem = document.createElement("ul");
+    ulElem.classList.add("results");
+
+    matchCarBrands.forEach((carBrands) => {
+      const liElem = document.createElement("li");
+      liElem.innerText = carBrands;
+      liElem.onclick = selectCarBrand;
+      ulElem.appendChild(liElem);
+    });
+    document.body.appendChild(ulElem);
+  }
+
+  function run() {
+    searchElem.addEventListener("input", onInput);
+    document.addEventListener('click', clearResults);
+  }
+
+  run();
 })();
