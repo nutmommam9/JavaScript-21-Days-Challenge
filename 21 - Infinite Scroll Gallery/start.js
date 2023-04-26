072a781501a1f58005f24e6f19a950f2a9b6d094
nutmommam9 <nutmommam_9@hotmail.com>
2023-04-26 19:17:14 +0700

    Challenge 21

--- 21 - Infinite Scroll Gallery/start.js
+++ 21 - Infinite Scroll Gallery/start.js
@@ -1,3 +1,50 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const KEY = "1EKSAPKeJOdT1ohwmqWDFnm4XL3_UbF0Cp1Ju8e0Rlw";
+  const loaderElem = document.querySelector(".loader");
+  let page = 1;
+
+  function showLoader() {
+    loaderElem.classList.add("visible");
+  }
+
+  function hideLoader(){
+    loaderElem.classList.remove('visible');
+  }
+
+  async function displayImages() {
+    showLoader();
+
+    const result = await fetch(
+      `https://api.unsplash.com/photos/?client_id=${KEY}&page=${page}`
+    );
+    const images = await result.json();
+
+    const galleryElem = document.querySelector(".gallery");
+
+    images.forEach((image) => {
+      const imgElem = document.createElement("img");
+      imgElem.src = image.urls.small;
+
+      galleryElem.appendChild(imgElem);
+    });
+
+    hideLoader();
+    page += 1;
+  }
+
+  function onScroll(){
+    const { scrollTop, clientHeight, scrollHeight} = document.documentElement;
+    
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+      displayImages();
+    }
+  }
+
+  function run() {
+    document.addEventListener('scroll', onScroll);
+    displayImages();
+  }
+
+  run();
 })();
