01f35aabc78253671118fe29cf565ff0fcd11d84
nutmommam9 <nutmommam_9@hotmail.com>
2023-04-26 18:20:00 +0700

    Challenge 18

--- 18 - What is prototype/start.js
+++ 18 - What is prototype/start.js
@@ -1,8 +1,33 @@
 (() => {
   // เริ่มเขียนโค้ด
 
-  // 1. Class vs Prototype 
+  // 1. Class vs Prototype
+  // class Person {
+
+  // }
+
+  // const thatton = new Person();
+  // console.log(thatton)
+
   // 2. What's prototype?
+  // const name = 'Thatton';
+  // console.log(name.__proto__);
+
+  // const arr = [];
+  // console.log(arr.__proto__);
+
   // 3. Prototype chain
+  // const name = 'Thatton';
+  // console.log(name.__proto__);
+  // console.log(name.toLocaleString());
+
   // 4. Extend a prototype
+  const name = "Thatton";
+  function sayHello(val) {
+    console.log(`Hello ${val}`);
+  }
+
+  String.prototype.sayHello = sayHello;
+  console.log(name.__proto__);
+  name.sayHello("World");
 })();
